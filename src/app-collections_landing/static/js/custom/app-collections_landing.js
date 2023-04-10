65f854bb1d8a9e4874af1f5a2fcd081be3dc1c13
ernest <e.sheremet@macbuildserver.com>
2023-04-10 14:37:43 +0300

    old app makets

--- src/app-collections_landing/static/js/custom/app-collections_landing.js
+++ src/app-collections_landing/static/js/custom/app-collections_landing.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.addEventListener('DOMContentLoaded', () => {
   // ------ FIX PORT VIEW -----
   function calculateVh() {
     const vh = window.innerHeight * 0.01;
@@ -30,6 +30,26 @@ window.onload = () => {
     observer.observe(e);
   })
 
+  // ---- ALERT ----
+  const alert = {
+    listener: document.getElementsByClassName('ColLP__promocode__button')[0],
+    dom: document.getElementsByClassName('ColLP__promocode__alert')[0],
+    promocode: document.querySelectorAll('.ColLP__promocode__desk span')[1].innerHTML,
+    timer: null,
+
+    init() {
+      this.listener.addEventListener('click', () => {
+        navigator.clipboard.writeText(alert.promocode);
+        alert.dom.classList.add('ColLP__promocode__alert_isVisible');
+        alert.timer = !alert.timer ? setTimeout(() => {
+          alert.dom.classList.remove('ColLP__promocode__alert_isVisible');
+          alert.timer = null;
+        }, 4000) : alert.timer;
+      })
+    }
+  };
+  alert.init();
+
   // ---- SWIPER ------
 
   new Swiper('.ColLP__comments__cards', {
@@ -49,5 +69,42 @@ window.onload = () => {
     },
   });
 
+  // ---- GA SENDER ------
+  const eventCat = window.location.pathname.slice(1, -1).replaceAll('/', '_');
+  ga('send', 'event', eventCat, 'user_page_load', `{"timestamp":"${getTimestamp()}"}`); // page is loaded
 
-};
+  // scroll events
+  const sendScrollAnalitics = (entries) => {
+    entries.forEach((e) => {
+      if(e.isIntersecting) {
+        const timestamp = getTimestamp();
+        const scrollDepth = e.target.dataset.scrolldepth;
+        if(scrollDepth != 0) {
+          ga('send', 'event', eventCat, 'user_scroll_page',
+            `{"timestamp":"${timestamp}", "scroll_depth":"${scrollDepth}"}`
+          );
+          e.target.dataset.scrolldepth = 0;
+        };
+      }
+    })
+  };
+
+  const GAobserver = new IntersectionObserver(sendScrollAnalitics);
+
+  [...document.getElementsByClassName('js-gaScrollEvent')].forEach(e => {
+    GAobserver.observe(e);
+  });
+
+  // tap events
+  [...document.getElementsByClassName('js-gaEvent')].forEach(e => {
+    const eventName = e.dataset.eventName;
+    const appName = e.dataset.appName;
+    e.addEventListener('click', () => {
+      if(appName) {
+        ga('send', 'event', eventCat, eventName, `{"timestamp":"${getTimestamp()}", "application":"${appName}"}`)
+      } else {
+        ga('send', 'event', eventCat, eventName, `{"timestamp":"${getTimestamp()}"}`)
+      }
+    })
+  });
+});
